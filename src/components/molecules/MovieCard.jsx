5c69d328db8fd7165a8175c334b80c09a28db18d
Dregert <rui.soares282@gmail.com>
2024-01-18 16:53:17 +0100

    components with hooks

--- src/components/molecules/MovieCard.jsx
+++ src/components/molecules/MovieCard.jsx
@@ -1,7 +1,21 @@
+import Modal from "../organismes/Modal.jsx";
+import {useState} from "react";
+import MoviePreview from "../organismes/MoviePreview.jsx";
+
 const MovieCard = ({ posterUrl, title, id, backdrop_path }) => {
+    const [showModal, setShowModal] = useState(false);
+
+    const openModal = () => {
+      setShowModal(true);
+    }
+
+    const closeModal = () => {
+      setShowModal(false);
+    }
+
     return (
         <>
-            <div className="cursor-pointer w-60 min-w-60 max-sm:w-40 max-sm:min-w-40 rounded overflow-hidden shadow-sm relative mx-3 hover:shadow-lg hover:ring-1 hover:shadow-blue-300 transition-all duration-700">
+            <div onClick={openModal} className="cursor-pointer w-60 min-w-60 max-sm:w-40 max-sm:min-w-40 rounded overflow-hidden shadow-sm relative mx-3 hover:shadow-lg hover:ring-1 hover:shadow-blue-300 transition-all duration-700">
                 <img
                     className="w-full hover:grayscale-[25%] grayscale-0 transition-all duration-700"
                     src={`https://image.tmdb.org/t/p/original/${posterUrl}`}
@@ -12,6 +26,12 @@ const MovieCard = ({ posterUrl, title, id, backdrop_path }) => {
                 <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black to-transparent h-20" />
                 <div className="absolute bottom-0 left-0 p-4 text-white">{title}</div>
             </div>
+            {showModal && (
+              <Modal onClose={closeModal}>
+                <MoviePreview backdrop_path={backdrop_path} id={id} title={title}/>
+              </Modal>
+            )}
+
         </>
     );
 };
